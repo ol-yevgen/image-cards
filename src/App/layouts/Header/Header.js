10b524832305a74bbc7e0a1ee950cbaa6674e37d
Oliinyk Yevgenii <oliinykyevgenii@gmail.com>
2023-05-13 19:50:00 +0300

    header with date, clock and number of cards

--- src/App/layouts/Header/Header.js
+++ src/App/layouts/Header/Header.js
@@ -1,16 +1,25 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useSelector } from "react-redux";
+import moment from "moment";
 
 export const Header = () => {
-    const [date, setDate] = useState()
+    const [date, setDate] = useState(moment().format('Do MMMM YYYY, h:mm:ss'))
     const cards = useSelector(state => state.cards.cards)
-    const cardsLength = cards.length
+    const cardsLength = cards.length;
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setDate(moment().format('Do MMMM YYYY, h:mm:ss a'))
+        })
+
+        return () => clearInterval(interval)
+    }, [date])
 
     return (
         <header>
             <div className="header">
                 <span className="cards">{cardsLength}</span>
-                <span></span>
+                <span>{date}</span>
             </div>
         </header>
     )
