59dbe57a80fc5f9e7201810368f7bbf8fdd4e7d5
Oliinyk Yevgenii <oliinykyevgenii@gmail.com>
2023-05-15 11:27:52 +0300

    images as background

--- src/components/CardItem/Card.js
+++ src/components/CardItem/Card.js
@@ -4,21 +4,25 @@ import { useDispatch } from "react-redux";
 import { useImageLazyLoading } from "../../hooks/useImageLazyLoading";
 import placeholder from "../../App/assets/placeholder.png"
 
-export const Card = ({ image, openCloseModal, chooseModalImage}) => {
+export const Card = ({ image, openCloseModal, chooseModalImage }) => {
     const dispatch = useDispatch();
     const imageLazyLoading = useImageLazyLoading(image)
-    
+
     const modal = useCallback(() => {
         dispatch(openCloseModal())
         dispatch(chooseModalImage(image))
     }, [dispatch, image, openCloseModal, chooseModalImage])
-    
-   return(
-       <li
-           className="card"
-           onClick={modal}
-       >
-           <img src={imageLazyLoading || placeholder} alt="img" />
-       </li>
-   )
+
+    return (
+        <li
+            className="card"
+            onClick={modal}
+        >
+            <div
+                className="card__image"
+                style={{ backgroundImage: `url(${imageLazyLoading || placeholder})` }}
+            >
+            </div>
+        </li>
+    )
 }
